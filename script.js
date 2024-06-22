559ed6decd2c54e2e0f8192aa9c39a854cce0a91
AhsanIlahi0 <ahsanilahi865@gmail.com>
2024-06-22 20:26:25 +0500

    second commit

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ function color(clickedId) {
     // Add the active class to the clicked h7 element
     document.getElementById(clickedId).classList.add('active');
 }
-if(window.innerWidth<=768){
-    this.document.getElementById("logo").classList.add("logo-margin");
-}
 window.addEventListener('scroll', function() {
     var navbar = document.getElementById('navbar');
     if (window.scrollY > 100) { // Adjust the value as needed
@@ -22,11 +19,13 @@ window.addEventListener('scroll', function() {
 });
 function openModal() {
     var modal = document.getElementById('modal');
+    var modal_c = document.getElementById('modal-content');
     modal.classList.add('show');
     modal.style.display = 'block';
     setTimeout(() => {
         modal.style.opacity = '1';
-    }, 10); // Slight delay to trigger CSS transition
+    }, 100); // Slight delay to trigger CSS transition
+    // modal_c.classList.add('new');
 }
 // Function to close the modal
 function closeModal() {
@@ -35,7 +34,9 @@ function closeModal() {
     setTimeout(() => {
         modal.classList.remove('show');
         modal.style.display = 'none';
-    }, 300); // Timeout to match the CSS transition duration
+    }, 200); // Timeout to match the CSS transition duration
+    document.getElementById('navbar').classList.remove('new');
+
 }
 // Close the modal when the user clicks anywhere outside of the modal
 window.onclick = function(event) {
